4b2763c1b8cdf94ec3aaffa7cc1b48b0d4aae8e4
Vinícius Arré <viniciusarre@gmail.com>
2020-05-05 14:12:57 -0300

    Chore: implement a factorial for numbers up to 10000

---
+++ factorial/src/main.rs
@@ -0,0 +1,50 @@
+use std::io;
+extern crate num_bigint;
+extern crate num_traits;
+
+use num_bigint::BigUint;
+use num_traits::{Zero, One};
+
+fn main() {
+    loop {
+        println!("Input a number up to 10,000");
+        let mut number = String::new();
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to read line");
+        let number: usize = match number.trim().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("Please input a valid number");
+                0
+            }
+        };
+
+        if number > 10_000 {
+            println!("Number too big, please, try again!");
+            continue;
+        } else if number == 0 {
+            continue;
+        }
+
+        
+
+        let factorial = factorial(number);
+        println!("Factorial of {} is: {} ", number, factorial);
+    }
+   
+}
+
+fn factorial (n: usize) -> BigUint {
+    if n == Zero::zero() {
+        One::one()
+    } else {
+        factorial(n - 1) * n
+    }
+// Another solution (non-recursive):
+    // let mut result :u128 = 1;
+    // for i in (1..n).rev() {
+    //     result *= u128::from(i);
+    // }
+    // result
+}
