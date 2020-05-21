55314d60a35f93084dfbf87aeff58f562916c10d
Vinícius Arré <viniciusarre@gmail.com>
2020-05-21 13:38:20 -0300

    Chore: implement christmas song using a combination of match and arrays

--- chapter-3-summary/src/main.rs
+++ chapter-3-summary/src/main.rs
@@ -77,5 +77,74 @@ fn second_option() {
 }
 
 fn third_option() {
-    println!("Third option chosen!");
+    print_lyrics();
+}
+
+fn get_gift_by_day (day: u8) -> String { 
+    match day {
+        1 => String::from("A Partridge in a Pear Tree"),
+        2 => String::from("Two Turtle Doves"),
+        3 => String::from("Three French Hens"),
+        4 => String::from("Four Calling Birds"),
+        5 => String::from("Five Golden Rings"),
+        6 => String::from("Six Geese a Laying"),
+        7 => String::from("Seven Swans a Swimming"),
+        8 => String::from("Eight Maids a Milking"),
+        9 => String::from("Nine Ladies Dancing"),
+        10 => String::from("Ten Lords a Leaping"),
+        11 => String::from("Eleven Pipers Piping"),
+        12 => String::from("Twelve Drummers Drumming"),
+        _ => String::from("Not found!")
+    }
+
+}
+
+fn get_lyrics_by_index (day: u8) -> String {
+    let mut lyrics = String::with_capacity(500);
+    if day == 1 {
+        lyrics.push_str("\n");
+        lyrics.push_str(&get_gift_by_day(day));
+    } else {
+        for i in (1..day + 1).rev() {
+            if i > 1 {
+                let gift = get_gift_by_day(i );
+                let line_break = "\n";
+                lyrics.push_str(line_break);
+                lyrics.push_str(&gift);
+            } else {
+                let gift = get_gift_by_day(i);
+                let line_break = "\nAnd ";
+                lyrics.push_str(line_break);
+                lyrics.push_str(&gift);   
+            }
+        }
+    }
+    lyrics
+}
+
+fn print_lyrics () {
+    for i in 1..13 {
+        let beginning = format!("{} {} {} ", "On the", get_day(i), "of Christmas my true love sent me:");;
+        let lyrics = get_lyrics_by_index(i);
+        println!("{} {} \n", beginning, lyrics)
+
+    }
+}
+
+fn get_day(index: u8) -> String {
+    match index {
+        1 => String::from("first"),
+        2 => String::from("second"),
+        3 => String::from("third"),
+        4 => String::from("fourth"),
+        5 => String::from("fifth"),
+        6 => String::from("sixth"),
+        7 => String::from("seventh"),
+        8 => String::from("eighth"),
+        9 => String::from("nineth"),
+        10 => String::from("tenth"),
+        11 => String::from("eleventh"),
+        12 => String::from("twelfth"),
+        _ => String::from("")
+    }
 }
